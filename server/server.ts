48a0f3f846a2a2a056f67f3baa5420fb529a8024
Devin Don <DevinDon@Foxmail.com>
2018-10-06 21:25:06 +0800

    Server: 服务器配置更新.

--- server/server.ts
+++ server/server.ts
@@ -1,14 +1,17 @@
 import * as Express from 'express';
 
-const app = Express();
+const app: Express.Application = Express();
 
 app.use(Express.static('./dist/calendar'));
 
-var server = app.listen(80, function () {
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
 
-  var host = server.address().address
-  var port = server.address().port
-
+const server = app.listen(80, function () {
   console.log(`请在浏览器导航栏中输入 localhost 来访问应用.`)
+});
 
+server.on('error', (error) => {
+  console.log(`Something wrong: ${error}`);
 });
