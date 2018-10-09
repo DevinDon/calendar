c83b2e83260e7f5b6beb5718f5350905d4c86f0e
Han Dashuai <yemeiqinxian@foxmail.com>
2018-10-09 21:19:38 +0800

    Fixed: 内容更正.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
-import * as Express from 'express';
-const app = Express();
+"use strict";
+exports.__esModule = true;
+var Express = require("express");
+var app = Express();
 app.use(Express.static('./dist/calendar'));
-const server = app.listen(80, function () {
-    console.log(`请在浏览器导航栏中输入 localhost 来访问应用.`);
+var server = app.listen(80, function () {
+    console.log("\u8BF7\u5728\u6D4F\u89C8\u5668\u5BFC\u822A\u680F\u4E2D\u8F93\u5165 localhost \u6765\u8BBF\u95EE\u5E94\u7528.");
 });
-server.on('error', (error) => {
-    console.log(`Something wrong: ${error}`);
+server.on('error', function (error) {
+    console.log("Something wrong: " + error);
 });
